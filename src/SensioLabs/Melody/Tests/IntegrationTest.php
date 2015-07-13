3eb00e0be760246069c3818687bba6d420f7aa78
ArthurHlt <arthurh.halet@gmail.com>
2015-07-13 23:44:31 +0100

    add tests

--- src/SensioLabs/Melody/Tests/IntegrationTest.php
+++ src/SensioLabs/Melody/Tests/IntegrationTest.php
@@ -11,18 +11,6 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
 {
     private $fs;
 
-    protected function setUp()
-    {
-        $this->fs = new Filesystem();
-        $this->cleanCache();
-    }
-
-    protected function tearDown()
-    {
-        $this->cleanCache();
-        $this->fs = null;
-    }
-
     public function testRunWithDefaultOption()
     {
         $output = $this->melodyRun('hello-world.php');
@@ -30,6 +18,37 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('Updating dependencies (including require-dev)', $output);
         $this->assertContains('Installing twig/twig (v1.16.0)', $output);
         $this->assertContains('Hello world', $output);
+    }
+
+    private function melodyRun($fixture, array $options = array())
+    {
+        $melody = new Melody();
+
+        $filename = $this->getFixtureFile($fixture);
+
+        $options = array_replace(array(
+            'prefer_source' => false,
+            'no_cache' => false,
+        ), $options);
+
+        $configuration = new RunConfiguration($options['no_cache'], $options['prefer_source']);
+
+        $output = null;
+        $cliExecutor = function (Process $process, $useProcessHelper) use (&$output) {
+            $process->setTty(false);
+            $process->mustRun(function ($type, $text) use (&$output) {
+                $output .= $text;
+            });
+        };
+
+        $melody->run($filename, array(), $configuration, $cliExecutor);
+
+        return $output;
+    }
+
+    private function getFixtureFile($fixtureName)
+    {
+        return sprintf('%s/Integration/%s', __DIR__, $fixtureName);
     }
 
     public function testRunWithShebang()
@@ -45,6 +64,13 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
     {
         $this->melodyRun('hello-world.php');
         $output = $this->melodyRun('hello-world.php');
+        $this->assertSame('Hello world', $output);
+    }
+
+    public function testRunWithConstraints()
+    {
+        $this->melodyRun('hello-world-with-constraints.php');
+        $output = $this->melodyRun('hello-world-with-constraints.php');
         $this->assertSame('Hello world', $output);
     }
 
@@ -118,39 +144,20 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('memory_limit=42M', $output);
     }
 
-    private function melodyRun($fixture, array $options = array())
+    protected function setUp()
     {
-        $melody = new Melody();
-
-        $filename = $this->getFixtureFile($fixture);
-
-        $options = array_replace(array(
-            'prefer_source' => false,
-            'no_cache' => false,
-        ), $options);
-
-        $configuration = new RunConfiguration($options['no_cache'], $options['prefer_source']);
-
-        $output = null;
-        $cliExecutor = function (Process $process, $useProcessHelper) use (&$output) {
-            $process->setTty(false);
-            $process->mustRun(function ($type, $text) use (&$output) {
-                $output .= $text;
-            });
-        };
-
-        $melody->run($filename, array(), $configuration, $cliExecutor);
-
-        return $output;
+        $this->fs = new Filesystem();
+        $this->cleanCache();
     }
 
     private function cleanCache()
     {
-        $this->fs->remove(sys_get_temp_dir().'/melody');
+        $this->fs->remove(sys_get_temp_dir() . '/melody');
     }
 
-    private function getFixtureFile($fixtureName)
+    protected function tearDown()
     {
-        return sprintf('%s/Integration/%s', __DIR__, $fixtureName);
+        $this->cleanCache();
+        $this->fs = null;
     }
 }
